531c8d1fddf33bc0ef01149291c2c0071d4f6919
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-04-04 11:46:45 +0530

    Refactor ContractTest. Move Emulator Container logic to a separate class

--- src/test/kotlin/com/example/order/ContractTest.kt
+++ src/test/kotlin/com/example/order/ContractTest.kt
@@ -1,10 +1,6 @@
 package com.example.order
 
-import com.github.dockerjava.api.command.CreateContainerCmd
-import com.github.dockerjava.api.model.ExposedPort
-import com.github.dockerjava.api.model.HostConfig
-import com.github.dockerjava.api.model.PortBinding
-import com.github.dockerjava.api.model.Ports
+import com.example.order.utils.PubSubEmulator
 import `in`.specmatic.googlepubsub.mock.GooglePubSubMock
 import `in`.specmatic.googlepubsub.mock.SpecmaticGooglePubSubTestBase
 import org.assertj.core.api.Assertions.assertThat
@@ -12,28 +8,27 @@ import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.springframework.boot.runApplication
 import org.springframework.context.ConfigurableApplicationContext
-import org.testcontainers.containers.GenericContainer
-import org.testcontainers.containers.wait.strategy.Wait
-import java.util.function.Consumer
 
 class ContractTest : SpecmaticGooglePubSubTestBase() {
 
     companion object {
 
         private const val PROJECT_ID = "pub-sub-demo-414308"
+        private const val PLACE_ORDER_TOPIC = "place-order"
+        private const val PROCESS_ORDER_TOPIC = "process-order"
+        private const val NOTIFICATION_TOPIC = "notification"
+
         private lateinit var context: ConfigurableApplicationContext
 
-        private const val EMULATOR_PORT = 8085
-        private const val TOPIC_CREATION_LOG = "Topics creation completed. Emulator is ready.."
-        private val emulator = createEmulatorContainer()
+        private val pubSubEmulator = PubSubEmulator(
+            projectId = PROJECT_ID,
+            topics = listOf(PLACE_ORDER_TOPIC, PROCESS_ORDER_TOPIC, NOTIFICATION_TOPIC),
+        )
 
         @JvmStatic
         @BeforeAll
         fun setUp() {
-            emulator.start()
-            emulator.execInContainer("/bin/bash", "-c", createTopicsCommand())
-            emulator.waitingFor(Wait.forLogMessage(TOPIC_CREATION_LOG, 1))
-
+            pubSubEmulator.start()
             googlePubSubMock = GooglePubSubMock.connectWithBroker(PROJECT_ID)
             context = runApplication<ProductServiceApplication>()
             context.getBean(ProductServiceApplication::class.java).run()
@@ -45,37 +40,7 @@ class ContractTest : SpecmaticGooglePubSubTestBase() {
             context.stop()
             val result = googlePubSubMock.stop()
             assertThat(result.success).withFailMessage(result.errors.joinToString()).isTrue
-            emulator.stop()
-        }
-
-        private fun createEmulatorContainer() : GenericContainer<*> {
-            val bindContainerPortToHost = Consumer<CreateContainerCmd> {
-                it.withHostConfig(
-                    HostConfig().withPortBindings(
-                        PortBinding(Ports.Binding.bindPort(EMULATOR_PORT), ExposedPort(EMULATOR_PORT))
-                    )
-                )
-            }
-
-            return GenericContainer(
-                "gcr.io/google.com/cloudsdktool/google-cloud-cli:emulators"
-            ).withExposedPorts(EMULATOR_PORT)
-                .withCreateContainerCmdModifier(bindContainerPortToHost)
-                .withEnv("PUBSUB_EMULATOR_HOST", "localhost:$EMULATOR_PORT")
-                .withCommand("gcloud beta emulators pubsub start --project=$PROJECT_ID --host-port=0.0.0.0:$EMULATOR_PORT")
-        }
-
-        private fun createTopicsCommand() : String {
-            val createTopicUrl = "http://localhost:$EMULATOR_PORT/v1/projects/${PROJECT_ID}/topics"
-            val script = StringBuilder()
-            val topics = listOf("place-order", "process-order", "notification")
-
-            for(topic in topics) {
-                script.append("curl -X PUT $createTopicUrl/$topic\n")
-            }
-            script.append("echo $TOPIC_CREATION_LOG\n")
-
-            return script.toString()
+            pubSubEmulator.stop()
         }
     }
 }
---
+++ src/test/kotlin/com/example/order/utils/PubSubEmulator.kt
@@ -0,0 +1,65 @@
+package com.example.order.utils
+
+import com.github.dockerjava.api.command.CreateContainerCmd
+import com.github.dockerjava.api.model.ExposedPort
+import com.github.dockerjava.api.model.HostConfig
+import com.github.dockerjava.api.model.PortBinding
+import com.github.dockerjava.api.model.Ports
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.wait.strategy.Wait
+import java.util.function.Consumer
+
+class PubSubEmulator(
+    private val projectId: String,
+    private val topics: List<String> = emptyList(),
+) {
+    private val port = 8085
+    private val host = "localhost:$port"
+    private val startupMessage = "PubSub Emulator is ready."
+
+    private val emulator = createEmulatorContainer()
+
+    fun start() {
+        emulator.start()
+        createTopics()
+        logStartupMessage()
+        emulator.waitingFor(Wait.forLogMessage(startupMessage, 1))
+    }
+
+    fun stop() = emulator.stop()
+
+    private fun createEmulatorContainer(): GenericContainer<*> {
+        val bindContainerPortToHost = Consumer<CreateContainerCmd> {
+            it.withHostConfig(
+                HostConfig().withPortBindings(
+                    PortBinding(Ports.Binding.bindPort(port), ExposedPort(port))
+                )
+            )
+        }
+
+        return GenericContainer(
+            "gcr.io/google.com/cloudsdktool/google-cloud-cli:emulators"
+        ).withExposedPorts(port)
+            .withCreateContainerCmdModifier(bindContainerPortToHost)
+            .withEnv("PUBSUB_EMULATOR_HOST", host)
+            .withCommand("gcloud beta emulators pubsub start --project=$projectId --host-port=0.0.0.0:$port")
+    }
+
+    private fun createTopics() {
+        val createTopicUrl = "http://$host/v1/projects/$projectId/topics"
+
+        topics.forEach { topic ->
+            emulator.execInContainer(
+                "/bin/bash",
+                "-c",
+                "curl -X PUT $createTopicUrl/$topic"
+            )
+        }
+    }
+
+    private fun logStartupMessage() = emulator.execInContainer(
+        "/bin/bash",
+        "-c",
+        "echo $startupMessage"
+    )
+}
