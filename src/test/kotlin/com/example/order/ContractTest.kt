4511e21c8a6d1cc5f63c3416122775c67c98b45d
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2025-04-14 13:03:02 +0530

    Update the specmatic-googlepubsub jar

--- src/test/kotlin/com/example/order/ContractTest.kt
+++ src/test/kotlin/com/example/order/ContractTest.kt
@@ -3,7 +3,6 @@ package com.example.order
 import com.example.order.utils.PubSubEmulator
 import io.specmatic.googlepubsub.mock.GooglePubSubMock
 import io.specmatic.googlepubsub.mock.SpecmaticGooglePubSubTestBase
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.springframework.boot.runApplication
@@ -13,6 +12,7 @@ class ContractTest : SpecmaticGooglePubSubTestBase() {
 
     companion object {
         private const val PROJECT_ID = "pub-sub-demo-414308"
+        private const val SHUTDOWN_TIMEOUT_FOR_MOCK_IN_MS = 1000
         private lateinit var context: ConfigurableApplicationContext
 
         private val pubSubEmulator = PubSubEmulator(projectId = PROJECT_ID)
@@ -29,8 +29,7 @@ class ContractTest : SpecmaticGooglePubSubTestBase() {
         @AfterAll
         fun tearDown() {
             context.stop()
-            val result = googlePubSubMock.stop()
-            assertThat(result.success).withFailMessage(result.errors.joinToString()).isTrue
+            googlePubSubMock.stop(SHUTDOWN_TIMEOUT_FOR_MOCK_IN_MS)
             pubSubEmulator.stop()
         }
     }
